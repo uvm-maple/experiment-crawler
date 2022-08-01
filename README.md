1256578da62ade0c7c9889894319a7ccda994fd7
Emma Tosch <Emma.Tosch@uvm.edu>
2022-08-01 12:57:54 -0400

    updated example sliding window code

--- README.md
+++ README.md
@@ -4,3 +4,14 @@
 2. Load the virtual environment
 3. Install dependencies: `pip install -r REQUIREMENTS.txt`
 4. Iterate frequently by running from the top-level of the repository: `python experiment-crawler/src/expcrawler.py`
+
+# Getting started
+
+Launch your VS Code from the command line to ensure that (a) your paths will be correct and (b) that VS Code will treat the repository as a workspace:
+
+```
+cd </your/local/path/to/the/repo>
+code . 
+```
+
+If this does not work because calling `code` from the command line doesn't work (i.e., `which code` does not print anything), then you will need to [enable launch from the command line](https://code.visualstudio.com/docs/setup/mac).
--- experiment-crawler/src/expcrawler.py
+++ experiment-crawler/src/expcrawler.py
@@ -1,26 +1,102 @@
-import requests
-import lxml
-from bs4 import BeautifulSoup
+import argparse
+import gzip
+import os
+import re
 
-# Boostrap with search engines by domain
-# Start with a bound name; loop through text later
+# Important things to learn:
+# How to read in and manipulate files (reading from a file advances the location of where you are reading from)
+# Using regular expressions to search through text data (here, in bytes)
+# Be mindful about differences between bytes and strings
 
-# Basic instructions from here: https://www.topcoder.com/thrive/articles/web-crawler-in-python
+def sliding_seek(f, delim):
+  # reads until the delimiter, returning everything until just before the 
+  # delimiter. Advances file reader to start just after the delimiter
+  window_size = len(delim) * 2
+  retval = b""
 
-headers = {
-  'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36 QIHU 360SE'
-} 
-# parameterize this query in the future (make news a variable)
-# Note that you can copy the url below and drop it into the browser
-f = requests.get('https://www.google.com/search?q=news', headers=headers)
+  while True:
+    # if __debug__:
+    #   print("Requesting {} bytes".format(window_size))
 
-soup = BeautifulSoup(f.content,'lxml')
-# I found the cite tag by:
-# 1. going to the section that I might want to expand in Chrome
-# 2. right clicking on that section and selecting 'Inspect Element"
-# 3. scrolling through the HTML
-#
-# You should play around with this more, following the example linked above. 
-# Track what you see and what you try in a notes file 
-results = soup.find('cite')
-print(results)
+    # this will store bytes from location 0 in the file stream 
+    # until some n, which we are requesting to be window_size, but may not be
+    window = f.peek(window_size)
+    
+    # if __debug__:
+    #   print("\tReceived {} bytes.".format(len(window)))
+
+    if delim in window: 
+        # if we find the delimiter in the string, grab the index of 
+        # the first instance of the delimiter
+        index = window.index(delim)
+        # print(index, "\n", delim, "\n", window, "\nWINDOW STARTING AT\n", window[:index], 
+        #   "\nWINDOW ENDING AT", window[index:])
+        # exit(0)
+        # append everything just read to the return value and advance the file reader
+        to_concat = f.read(index)
+        retval += to_concat
+        # advance file reader to the location
+        return retval
+
+    else:
+        # delimiter not yet found
+        retval += window[:len(delim)] 
+        # advance the file reader 
+        f.read(len(delim))
+
+
+
+# This has to be the last basic block in the file
+if __name__ == "__main__":
+
+  data_dir = "experiment-crawler/data"
+  delim = b"Content-Length"
+
+  # regular expression 
+  pat = re.compile(rb"Content-Length: [0-9]+\r\n")
+
+  # variables that are tracking information
+  mathrandom = 0
+  max_possible_pages = 32
+  possible_pages = max_possible_pages
+
+  for file in os.listdir(data_dir):
+    fname = os.sep.join([data_dir, file])
+    print("{} is {}MB".format(fname, int(os.path.getsize(fname) / (1024*1024))))
+
+    with gzip.open(fname, 'rb') as f:
+      while max_possible_pages > 0 : 
+        
+        first_header = sliding_seek(f, b"\r\n\r\n\r\n")
+        with open("{}_header1.out".format(max_possible_pages), "wb") as outfile:
+          outfile.write(first_header)
+
+        assert len(first_header) > 1
+        
+        if delim in first_header:
+
+          content_length = int(pat.findall(first_header)[0][len(delim)+1:-2])    
+          # print("reading next {} bytes:".format(content_length))
+
+          second_header = f.read(content_length)
+          with open("{}_header2.out".format(max_possible_pages), "wb") as outfile:
+            outfile.write(second_header)
+
+          try:
+            html_length = int(pat.findall(second_header)[0][len(delim)+1:-2])
+            page = f.read(html_length)
+            with open("{}_body.out".format(max_possible_pages), "wb") as outfile:
+              outfile.write(page)
+            if b"Math.random" in page:
+              mathrandom += 1
+
+          except: 
+            with open ("{}.err".format(max_possible_pages), "wb") as err:
+              err.write(second_header)
+        else:
+          print("Delimiter not in first header!!")
+
+        max_possible_pages -= 1
+
+  assert max_possible_pages == 0      
+  print("{}/{} pages use Math.random".format(mathrandom, possible_pages))
